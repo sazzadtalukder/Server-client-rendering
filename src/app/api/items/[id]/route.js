a9b20250066604412c0b6595bd86241b83df8d08
Md. Sazzad Talukder <sazzadtalukdercseiu@gmail.com>
2025-05-26 14:48:01 +0600

    Patch done in database

--- src/app/api/items/[id]/route.js
+++ src/app/api/items/[id]/route.js
@@ -2,7 +2,7 @@ import dbConnect from "@/lib/dbConnect";
 import { ObjectId } from "mongodb";
 
 export async function GET(req,{params}) {
-  const p = await params;
+//   const p = await params;
   const singleData = await dbConnect("practice_data").findOne({_id : new ObjectId(params.id)})
   console.log(p)
  
@@ -14,9 +14,18 @@ export async function DELETE(req,params) {
  
   return Response.json({ params : p })
 }
-export async function PATCH(req,params) {
-  const p = await params;
-  console.log(p)
+export async function PATCH(req,{params}) {
+  const updatedData =  await req.json();
+  const updateDoc = {
+    $set : {
+        ...updatedData
+    }
+  }
+  const filter = {
+    _id : new ObjectId(params.id)
+  }
+  const result = await dbConnect("practice_data").updateOne(filter,updateDoc)
+
  
-  return Response.json({ params : p })
+  return Response.json(result)
 }
