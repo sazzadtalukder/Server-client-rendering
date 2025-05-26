c8991c2073254d14b477b1d75ce8088eac1297fa
Md. Sazzad Talukder <sazzadtalukdercseiu@gmail.com>
2025-05-26 14:53:35 +0600

    Delete from database done

--- src/app/api/items/[id]/route.js
+++ src/app/api/items/[id]/route.js
@@ -4,15 +4,17 @@ import { ObjectId } from "mongodb";
 export async function GET(req,{params}) {
 //   const p = await params;
   const singleData = await dbConnect("practice_data").findOne({_id : new ObjectId(params.id)})
-  console.log(p)
+//   console.log(p)
  
   return Response.json(singleData)
 }
-export async function DELETE(req,params) {
-  const p = await params;
-  console.log(p)
+export async function DELETE(req,{params}) {
+  const filter = {
+    _id : new ObjectId(params.id)
+  }
+  const result = await dbConnect("practice_data").deleteOne(filter)
  
-  return Response.json({ params : p })
+  return Response.json(result)
 }
 export async function PATCH(req,{params}) {
   const updatedData =  await req.json();
