904a46b8ab241c7d29723b07ca9b71da492a7bed
Md. Sazzad Talukder <sazzadtalukdercseiu@gmail.com>
2025-05-26 14:40:22 +0600

    Get single data form database done

--- src/app/api/items/[id]/route.js
+++ src/app/api/items/[id]/route.js
@@ -1,8 +1,12 @@
-export async function GET(req,params) {
+import dbConnect from "@/lib/dbConnect";
+import { ObjectId } from "mongodb";
+
+export async function GET(req,{params}) {
   const p = await params;
+  const singleData = await dbConnect("practice_data").findOne({_id : new ObjectId(params.id)})
   console.log(p)
  
-  return Response.json({ params : p })
+  return Response.json(singleData)
 }
 export async function DELETE(req,params) {
   const p = await params;
